af92f01597901fd97688339c7c0e66864d46aba2
pampapathi73 <pampapathik32@gmail.com>
2021-06-13 13:23:07 +0530

    [pampapathi] Added: Create simple linked list

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,6 +7,10 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linked list implementation");
+            LikedLists list = new LikedLists();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
         }
     }
 }
