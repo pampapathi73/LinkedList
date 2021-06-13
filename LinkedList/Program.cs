837711664d8723f62c39dc71d654fcf32bf0ab37
pampapathi73 <pampapathik32@gmail.com>
2021-06-13 17:31:55 +0530

    [pampapathi] Added: list in ascending order

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,33 +7,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linked list implementation");
-            LikedLists list = new LikedLists();
+            SortedLinkedList list = new SortedLinkedList();
             list.Add(56);
+            list.Add(30);
+            list.Add(40);
             list.Add(70);
-            Console.WriteLine("The list before inserting 30 is : ");
+            Console.WriteLine("The ordered list after inserting 56,30,40,70 using sorted linked list is : ");
             list.Display();
-            list.InsertAtParticularPosition(2, 30);
-            Console.WriteLine("The list after inserting 30 is : ");
-            list.Display();
-            list.InsertAtParticularPosition(3, 40);
-            Console.WriteLine("The list after inserting 30 is : ");
-            list.Display();
-            list.RemoveFirstNode();
-            Console.WriteLine("The list after removing first node is : ");
-            list.Display();
-            list.RemoveLastNode();
-            Console.WriteLine("The list after removing last node is : ");
-            list.Display();
-            list.deleteNode(40);
-
-            Console.WriteLine("\nLinked List after Deletion of 40:");
-            list.Display();
-        
-
-            if (list.Search(list.head, 30))
-                Console.WriteLine("Yes");
-            else
-                Console.WriteLine("No");
 
         }
     }
---
+++ LinkedList/SortedLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class SortedLinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            Node current = head, previous = null;
+            while (current != null && current.data < node.data)
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (previous == null)
+                head = node;
+            else
+            {
+                previous.next = node;
+            }
+            node.next = current;
+
+        }
+        internal void Display()
+        {
+            if (this.head == null)
+                Console.WriteLine("The list is empty.");
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
